a8b047869a4af46fe4f9d5e7579a036acef8401c
weiwei <coderww@163.com>
2020-11-23 19:40:50 +0800

    更新库模块

--- mylib.py
+++ mylib.py
@@ -1,7 +1,21 @@
 import os
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 from openpyxl import load_workbook
+
+def GenCandidate(num_c, num_g, num):
+    cand_c = []
+    cand_g = []
+    while len(cand_c) < num:
+        tmp = random.randint(0, num_c)
+        if tmp not in cand_c:
+            cand_c.append(tmp)
+    while len(cand_g) < num:
+        tmp = random.randint(0, num_g)
+        if tmp not in cand_g:
+            cand_g.append(tmp)
+    return cand_c, cand_g
 
 # 结果可视化
 def Visualize(num, groups, index):  # 可以尝试在柱状图上带数据
@@ -16,7 +30,7 @@ def Visualize(num, groups, index):  # 可以尝试在柱状图上带数据
     plt.show()
 
 # 重命名文件夹中的文件
-def rename():
+def Rename():
     path = r'D:\20201103\20191218-01\pic\coal'
     cnt = 0
     filelist = os.listdir(path)  # 该文件夹下所有的文件（包括文件夹）
@@ -32,14 +46,14 @@ def rename():
         cnt += 1
 
 # 将信息记录到excel表中
-def record(sheetname, ore):
+def Record(sheetname, ore):
     wb = load_workbook('dataset.xlsx')
     ws = wb[sheetname]
     ws.append(ore)
     wb.save('dataset.xlsx')
 
 # 在excel表中记录未被选择的图片
-def filter():
+def Filter():
     path = r'D:\coal-gangue\selected\gangue'
     filelist = os.listdir(path)
     for i in range(457):
