fdd3be36e7f07f078f3f89c6180697dd66c9c686
weiwei <coderww@163.com>
2020-11-26 10:00:32 +0800

    更新主函数模块（k次k折交叉检验）

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ import CrsVldt as cvd
 if __name__ == '__main__':
     # k次k折交叉检验
     times = 10
+    acc = []
+    pre = []
+    rec = []
     for i in range(times):
-        accuary, precision, recall = cvd.CrossVld(times)
-
-    mylib.Visualize(accuary, times, i)
-    mylib.Visualize(precision, times, i)
-    mylib.Visualize(recall, times, i)
+        accuracy, precision, recall = cvd.CrossVld(times)
+        acc.append(sum(accuracy)/len(accuracy))
+        pre.append(sum(precision)/len(precision))
+        rec.append(sum(recall)/len(recall))
+    mylib.Visualize(acc, times)
+    mylib.Visualize(pre, times)
+    mylib.Visualize(rec, times)
